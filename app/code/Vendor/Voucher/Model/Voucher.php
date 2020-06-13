fefda473b2b54f62c70e49e753d03c35ec0bda76
Alexandr <5b4e5f58555f4e5b@gmail.com>
2020-06-13 14:53:42 +0000

    5-3-Patch_Plugin_Observer_EA: adding Patch, Plugins, Observer, EA

--- app/code/Vendor/Voucher/Model/Voucher.php
+++ app/code/Vendor/Voucher/Model/Voucher.php
@@ -1,13 +1,14 @@
 <?php
 
-
 namespace Vendor\Voucher\Model;
 
 use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\AbstractExtensibleModel;
+use Vendor\Voucher\Api\Data\VoucherExtensionInterface;
+use Vendor\Voucher\Api\Data\VoucherInterface;
 use Vendor\Voucher\Model\ResourceModel\Voucher as ResourceModel;
 
-class Voucher extends AbstractModel implements IdentityInterface
+class Voucher extends AbstractExtensibleModel implements IdentityInterface, VoucherInterface
 {
     const CACHE_TAG = 'voucher';
 
@@ -30,34 +31,67 @@ class Voucher extends AbstractModel implements IdentityInterface
         return [];
     }
 
-
+    /**
+     * @param $customer_id
+     * @return Voucher
+     */
     public function setCustomerId($customer_id)
     {
         return $this->setData('customer_id', $customer_id);
     }
 
+    /**
+     * @return int|string
+     */
     public function getCustomerId()
     {
         return $this->getData('customer_id');
     }
 
+    /**
+     * @param $status_id
+     * @return Voucher
+     */
     public function setStatusId($status_id)
     {
         return $this->setData('status_id', $status_id);
     }
 
+    /**
+     * @return int|string
+     */
     public function getStatusId()
     {
         return $this->getData('status_id');
     }
 
+    /**
+     * @param $voucher_code
+     * @return Voucher
+     */
     public function setVoucherCode($voucher_code)
     {
         return $this->setData('voucher_code', $voucher_code);
     }
 
+    /**
+     * @return int|string
+     */
     public function getVoucherCode()
     {
         return $this->getData('voucher_code');
+    }
+
+    public function getExtensionAttributes()
+    {
+        return $this->_getExtensionAttributes();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setExtensionAttributes(VoucherExtensionInterface $extensionAttributes)
+    {
+        return $this->_setExtensionAttributes($extensionAttributes);
     }
 }
