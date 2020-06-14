72ffe348e226c758f465abd61b1841529455daf4
Alexandr <5b4e5f58555f4e5b@gmail.com>
2020-06-14 08:44:41 +0000

    5-3-Patch_Plugin_Observer_EA: VoucherBeforSavePlugin Fix

--- app/code/Vendor/Voucher/Plugin/VoucherBeforeSavePlugin.php
+++ app/code/Vendor/Voucher/Plugin/VoucherBeforeSavePlugin.php
@@ -2,31 +2,15 @@
 
 namespace Vendor\Voucher\Plugin;
 
-use Magento\Customer\Model\CustomerFactory as CustomerModel;
 use Vendor\Voucher\Model\Voucher as Subject;
 
 class VoucherBeforeSavePlugin
 {
     /**
-     * @var CustomerModel
-     */
-    private $customerModel;
-
-    /**
-     * VoucherAfterSetCustomerIdPlugin constructor.
-     * @param CustomerModel $customerModel
-     */
-    public function __construct(CustomerModel $customerModel)
-    {
-        $this->customerModel = $customerModel;
-    }
-
-    /**
      * @param Subject $subject
      */
     public function beforeSave(Subject $subject)
     {
-        $customer = $this->customerModel->create()->getId();
         $subject->setData('customer_id', $subject->getExtensionAttributes()->getCustomer()->getId());
     }
 }
